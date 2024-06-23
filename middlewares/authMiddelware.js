eafd79f0fd481d4fc45537b31168a60347f54527
sakshi04agrawal <sakshi04agrawal@example.com>
2024-06-23 16:32:48 +0530

    first commit

---
+++ middlewares/authMiddelware.js
@@ -0,0 +1,31 @@
+const JWT = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  try {
+    // const token = req.headers["authorization"].split(" ")[1];
+    let token;
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+      token = req.headers.authorization.split(' ')[1];
+    } else if (req.query && req.query.token) {
+      token = req.query.token;
+    }
+    JWT.verify(token, process.env.JWT_SECRET, (err, decode) => {
+      if (err) {
+        return res.status(401).send({
+          success: false,
+          message: "Auth Failed",
+        });
+      } else {
+        req.body.userId = decode.userId;
+        next();
+      }
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(401).send({
+      success: false,
+      error,
+      message: "Auth Failedd",
+    });
+  }
+};
